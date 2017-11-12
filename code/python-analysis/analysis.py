751e71ef53288746137724f3d6d9f0a4429b1236
skokov <sk1056@york.ac.uk>
2017-11-12 11:57:04 +0000

    Refactoring

--- code/python-analysis/analysis.py
+++ code/python-analysis/analysis.py
@@ -9,7 +9,7 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from arch.unitroot import ADF, PhillipsPerron, KPSS
 from code import data_utils
 
-OUTPUT_PATH = os.path.join(os.path.basename(os.path.realpath(__file__)), '..', '..', 'experiments', 'python-analysis')
+OUTPUT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'experiments', 'python-analysis')
 
 def plot_rolling_stats(data, window=12):
     rolling_mean = data.rolling(window=window).mean()
@@ -23,7 +23,7 @@ def plot_rolling_stats(data, window=12):
         plt.title(variable)
 
 
-def stationary_tests(data):
+def stationary_tests(data, country):
     results = pd.DataFrame(index=['ADF', 'PP', 'KPSS'],
                            columns=['t-stat', 'p-value', 'crit-val 1%', 'crit-val 5%', 'crit-val 10%', 'result'])
     for variable in data.columns:
@@ -41,26 +41,17 @@ def stationary_tests(data):
             results['crit-val 10%'][test_name] = test.critical_values['10%']
             results['result'][test_name] = test.alternative_hypothesis if test.pvalue < 0.05 else test.null_hypothesis
 
-        file_path = os.path.join(OUTPUT_PATH, 'stationary-tests', variable + '')
-        results.to_csv()
+        file_path = os.path.join(OUTPUT_PATH, 'stationary-tests', variable + '_' + country + '.csv')
+        results.to_csv(file_path,float_format='%.3f')
 
 
 def main():
     ea_data = data_utils.get_ea_data()
     us_data = data_utils.get_us_data()
 
-    stationary_tests(ea_data)
+    stationary_tests(ea_data, 'EA')
+    stationary_tests(us_data, 'US')
 
-    # test_stationarity(ea_data['CPI'].dropna(), 8)
-    #
-    # # plot_roll(ea_data, 12)
-    # decomp = seasonal_decompose(ea_data['CPI'])
-    # decomp.plot()
-    # plt.show()
-    # plt.plot(decomp.resid + decomp.seasonal + decomp.trend)
-    # plt.plot(ea_data['CPI'], '--r')
-    # plt.show()
-    # test_stationarity(decomp.resid.dropna(), 16)
     plt.show()
 
 
