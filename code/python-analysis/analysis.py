9ec23d045856fb5cb54399d334f8c537ca9ad24c
skokov <sk1056@york.ac.uk>
2017-11-12 14:35:27 +0000

    Added experiments utils.

--- code/python-analysis/analysis.py
+++ code/python-analysis/analysis.py
@@ -7,11 +7,15 @@ from pandas import DataFrame
 from statsmodels.tsa.stattools import adfuller, kpss
 from statsmodels.tsa.seasonal import seasonal_decompose
 from arch.unitroot import ADF, PhillipsPerron, KPSS
-from code import data_utils
+from code.utils import data_utils
+from code.utils.experiments_utils import expr_sub_dir, expr_file_name
 
-OUTPUT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'experiments', 'python-analysis')
+_EXPERIMENTS_DIR_NAME = 'python-analysis'
+ROLLING_STATISTICS_DIR = expr_sub_dir(_EXPERIMENTS_DIR_NAME, 'rolling-statistics')
+STATIONARY_TESTS_DIR = expr_sub_dir(_EXPERIMENTS_DIR_NAME, 'stationary-tests')
 
-def plot_rolling_stats(data, window=12):
+
+def plot_rolling_stats(data, window=12, country=None):
     rolling_mean = data.rolling(window=window).mean()
     rolling_std = data.rolling(window=window).std()
     for index, variable in enumerate(data.columns):
@@ -22,8 +26,12 @@ def plot_rolling_stats(data, window=12):
         plt.legend(loc='best')
         plt.title(variable)
 
+        if country:
+            file_path = os.path.join(ROLLING_STATISTICS_DIR, expr_file_name(country, variable, 'png'))
+            plt.savefig(file_path, bbox_inches='tight')
 
-def stationary_tests(data, country):
+
+def stationary_tests(data, country=None):
     results = pd.DataFrame(index=['ADF', 'PP', 'KPSS'],
                            columns=['t-stat', 'p-value', 'crit-val 1%', 'crit-val 5%', 'crit-val 10%', 'result'])
     for variable in data.columns:
@@ -41,16 +49,19 @@ def stationary_tests(data, country):
             results['crit-val 10%'][test_name] = test.critical_values['10%']
             results['result'][test_name] = test.alternative_hypothesis if test.pvalue < 0.05 else test.null_hypothesis
 
-        file_path = os.path.join(OUTPUT_PATH, 'stationary-tests', variable + '_' + country + '.csv')
-        results.to_csv(file_path,float_format='%.3f')
+        print '\n', variable, '\n', results, '\n'
+
+        if country:
+            file_path = os.path.join(STATIONARY_TESTS_DIR, expr_file_name(country, variable, 'csv'))
+            results.to_csv(file_path, float_format='%.3f')
 
 
 def main():
     ea_data = data_utils.get_ea_data()
     us_data = data_utils.get_us_data()
 
-    stationary_tests(ea_data, 'EA')
-    stationary_tests(us_data, 'US')
+    stationary_tests(ea_data, country='EA')
+    plot_rolling_stats(ea_data, country='EA')
 
     plt.show()
 
