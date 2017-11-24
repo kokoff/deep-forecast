e9a8344bb9996d6e7abdc6888d04a361abed7802
skokov <sk1056@york.ac.uk>
2017-11-24 20:22:58 +0000

    Better plotting and csv output files

--- r-scripts/arima.R
+++ r-scripts/arima.R
@@ -1,13 +1,70 @@
 output_dir = '/home/skokov/project/experiments/arma'
 
 
-ea_data = read.csv('/home/skokov/project/data/EA.csv', row.names = 1)
-us_data = read.csv('/home/skokov/project/data/US.csv', row.names = 1)
+ea_data = read.csv('/home/skokov/project/data/EA.csv', row.names = 1, check.names = FALSE)
+us_data = read.csv('/home/skokov/project/data/US.csv', row.names = 1, check.names = FALSE)
 data = list(EA=ea_data, US=us_data)
+
+  
+  
+  
+  # Multiple plot function
+  #
+  # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+  # - cols:   Number of columns in layout
+  # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+  #
+  # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+  # then plot 1 will go in the upper left, 2 will go in the upper right, and
+  # 3 will go all the way across the bottom.
+  #
+  multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+    library(grid)
+    
+    # Make a list from the ... arguments and plotlist
+    plots <- c(list(...), plotlist)
+    
+    numPlots = length(plots)
+    
+    # If layout is NULL, then use 'cols' to determine layout
+    if (is.null(layout)) {
+      # Make the panel
+      # ncol: Number of columns of plots
+      # nrow: Number of rows needed, calculated from # of cols
+      layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                       ncol = cols, nrow = ceiling(numPlots/cols))
+    }
+    
+    if (numPlots==1) {
+      print(plots[[1]])
+      
+    } else {
+      # Set up the page
+      grid.newpage()
+      pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+      
+      # Make each plot, in the correct location
+      for (i in 1:numPlots) {
+        # Get the i,j matrix positions of the regions that contain this subplot
+        matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+        
+        print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                        layout.pos.col = matchidx$col))
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
 
 names(data)
 
 library(forecast)
+library(ggplot2)
 
 out= list()
 
@@ -16,51 +73,85 @@ for (country in names(data)){
     print(country)
     print(name)
     
-
     
     series <- na.omit(ts(data = data[[country]][name], start = c(1998,1), frequency = 4))
-    train = head(series, -12)
-    test = tail(series, 12)
+    series.train = head(series, -12)
+    series.test = tail(series, 12)
     
     
-    res = auto.arima(train, stepwise = FALSE, approximation = FALSE)
+    res = auto.arima(series.train, stepwise = FALSE, approximation = FALSE)
     fcast = forecast(res, h = 12)
     newfit= Arima(series, model = res)
-    fit = fitted(newfit,h = 1)
+    prediction = fitted(newfit,h = 1)
+    
+    prediction.train = head(prediction, length(series.train))
+    prediction.test = tail(prediction, length(series.test))
+    
+    
+    model = paste('ARIMA(', res$arma[1], ',', res$arma[6], ',', res$arma[2], ')' ,
+                  '(', res$arma[3], ',', res$arma[7], ',', res$arma[4], ')',
+                  '[', res$arma[5], ']', sep='')
+    y_lab=paste(country, name, sep = ' ')
     
     
     
+    pdf(file = paste(output_dir, '/', country,'_', name, '_', 'Prediction', '.pdf', sep = ''))
     
-    png(filename = paste(output_dir, '/', country,'_', name,'.png', sep = ''), width=700, height = 1000)
-    par(mfrow=c(3,2))
+    p = autoplot(series, series='observed', ylab=y_lab) + autolayer(prediction.train, series = 'prediction train') + 
+      autolayer(prediction.test, series = 'prediction test') + ggtitle(paste('One step prediction from ', model, sep='')) + theme(legend.position="bottom") + 
+      theme(text = element_text(size=16))
+    print(p)
     
-    plot(series, col='red')
-    lines(fit, col='blue', lwd=2)
-    legend('topleft', legend = c('original', 'one step predict'), col=c('blue', 'red'), lty=1)
+    dev.off()
+
+    pdf(file = paste(output_dir, '/', country,'_', name, '_', 'Forecast', '.pdf', sep = ''))
     
-    plot(fcast, col=c('blue', 'blue', 'red'))
-    lines(series, col='red', lwd=1)
+    p = autoplot(fcast, ylab=y_lab) + autolayer(series, series='observed') + autolayer(fcast$mean, series='forecast') +
+      ggtitle(paste('Forecasts from ', model, sep = '')) + theme(legend.position="bottom") + 
+      theme(text = element_text(size=16))
+    print(p)
     
-    legend('topleft', legend = c('recursive forecast', 'original'), col=c('blue', 'red'), lty=1)
-    
-    plot(residuals(res))
-    plot(density(residuals(res)))
-    Acf(residuals(res))
-    Pacf(residuals(res))
     dev.off()
     
-    out = as.list(res$arma)
-    names(out) = c('p', 'd','q', 'P', 'D', 'Q', 'asd')
+    pdf(file = paste(output_dir, '/', country,'_', name, '_', 'Residuals', '.pdf', sep = ''))
     
-    out['Train MSE'] = mean((head(fit, length(train)) - train)^2)
-    out['Test MSE'] = mean((tail(fit, length(test)) - test)^2)
-    out['Test MSE forecast'] = mean((fcast$mean - test)^2)
+    checkresiduals(res, ylab=y_lab, theme=theme(text = element_text(size=16)))
     
-    out[] <- lapply(out,round,5)
-    write.csv(x = out,file = paste(output_dir, '/',country,'_', name,'.csv', sep = ''), quote = FALSE, row.names = FALSE)
+    dev.off()
+
+    
+    acc.train = c(accuracy(prediction.train, series.train))
+    acc.test = accuracy(prediction.test, series.test)
+    acc.fcast = accuracy(fcast, series.test)
+    
+    out=matrix(nrow = 3, ncol = 5)
+    rownames(out) <- c('Train', 'Test', 'Forecast')
+    colnames(out) <- c('MSE', 'RMSE', 'MAE', 'ME', 'ACF1')
+    
+    out['Train', 'MSE'] = mean((series.train - prediction.train)^2)
+    out['Train', 'RMSE'] = acc.train[2]
+    out['Train', 'MAE'] = acc.train[3]
+    out['Train', 'ME'] = acc.train[1]
+    out['Train', 'ACF1'] = acc.train[6]
+      
+    out['Test', 'MSE'] = mean((series.test - prediction.test)^2)
+    out['Test', 'RMSE'] = acc.test[2]
+    out['Test', 'MAE'] = acc.test[3]
+    out['Test', 'ME'] = acc.test[1]
+    out['Test', 'ACF1'] = acc.test[6]  
+    
+    out['Forecast', 'MSE'] = mean((series.test - fcast$mean)^2)
+    out['Forecast', 'RMSE'] = acc.fcast[2,2]
+    out['Forecast', 'MAE'] = acc.fcast[2,3]
+    out['Forecast', 'ME'] = acc.fcast[2,1]
+    out['Forecast', 'ACF1'] = acc.fcast[2,7]
+    
+    out <- round(out, 5)
+    output = cbind(rownames(out), out)
+    write.csv(x = output, file = paste(output_dir, '/',country,'_', name, '_', 'Accuracy', '.csv', sep = ''), quote = FALSE, row.names = FALSE)
     
     summary(res)
-    print('MSE Test:')
-    print(mean((fcast$mean - test)^2))
+    print(out)
   }
 }
+
