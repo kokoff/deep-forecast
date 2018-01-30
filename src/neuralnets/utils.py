1074b60a334a2c5e5f9af1a5a9a1a3e270f98071
skokov <sk1056@york.ac.uk>
2018-01-30 14:13:35 +0000

    Made changes to XY data function

--- src/neuralnets/utils.py
+++ src/neuralnets/utils.py
@@ -3,15 +3,34 @@ import pandas as pd
 
 
 # Returns x with appropriate lag values and y
-def get_xy_data(data, lags=1):
-    index = [(i, 'x' + str(j)) for i in data.columns for j in range(lags)]
+def get_xy_data(data, lags1=1, lags2=1):
+    # Transform X with approapriate lag values
+    index = [(i, 'x' + str(j)) for i in data.columns for j in range(lags1)]
     index = pd.MultiIndex.from_tuples(index, names=['variable', 'lag'])
     x = pd.DataFrame(np.zeros((len(data), len(index))), index=data.index, columns=index, dtype='float64')
+    for i in range(lags1):
+        x[[(j, 'x' + str(i)) for j in data.columns]] = data.shift(i + lags2)
 
-    for i in range(lags):
-        x[[(j, 'x' + str(i)) for j in data.columns]] = data.shift(-i)
+    x = x.tail(-lags1 - lags2 + 1)
 
-    x = x.head(-lags)
-    y = data.shift(-lags).head(-lags)
+    # Transform Y with approapriate lag values
+    index = [(i, 'y' + str(j)) for i in data.columns for j in range(lags2)]
+    index = pd.MultiIndex.from_tuples(index, names=['variable', 'lag'])
+    y = pd.DataFrame(np.zeros((len(data), len(index))), index=data.index, columns=index, dtype='float64')
+
+    for i in range(lags2):
+        y[[(j, 'y' + str(i)) for j in data.columns]] = data.shift(i)
+
+    y = y.tail(-lags1 - lags2 + 1)
 
     return x, y
+
+
+if __name__ == '__main__':
+    from src.utils import data_utils
+
+    data = data_utils.get_ea_data()[['CPI']]
+    X, Y = get_xy_data(data, 1, 2)
+
+    print data.head(), X.head(), Y.head()
+    print data.tail(), X.tail(), Y.tail()
