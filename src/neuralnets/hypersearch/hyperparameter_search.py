b2b42981ad57cfd22281f6ea381cdb680cd0806c
skokov <sk1056@york.ac.uk>
2018-02-17 17:48:50 +0000

    checkpoint before major changes

--- src/neuralnets/hypersearch/hyperparameter_search.py
+++ src/neuralnets/hypersearch/hyperparameter_search.py
@@ -12,129 +12,91 @@ from sklearn.model_selection import TimeSeriesSplit
 from src.neuralnets.forecast_models import ModelWrapper
 
 from src.neuralnets.hypersearch.results import MultiResult, SingleResult
+from sklearn.model_selection import cross_validate
+from src.neuralnets.model_selection.model_selection import ForecastRegressor
+from validation import ModelEvaluator
+from src.utils import data_utils
+from optunity import functions, search_spaces
+from optunity.constraints import wrap_constraints
+import sys
+from multiprocessing import Pool
+from optunity.parallel import create_pmap
+from optimizers import PSOptimizer
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-class Validator:
-    def __init__(self, f, x_train, y_train, x_val, y_val, runs, cross_validation=False):
-        self.x = pd.concat([x_train, x_val])
-        self.y = pd.concat([y_train, y_val])
-        self.runs = runs
-        self.build_fn = f
-        if not cross_validation:
-            l1 = [i for i in range(len(x_train))]
-            l2 = [i for i in range(len(x_val))]
-            self.split = [(l1, l2) for i in range(runs)]
-        else:
-            splitter = TimeSeriesSplit(runs)
-            self.split = list(splitter.split(self.x, self.y))
+class Logger:
+    def __init__(self):
+        self._log = OrderedDict()
+        self.best_params = None
+        self.best_results = {'train': np.inf, 'val': np.inf}
+        self.log_initialised = False
 
-        self.data = []
-        for i, j in self.split:
-            temp = []
-            temp.append(self.x.iloc[i])
-            temp.append(self.y.iloc[i])
-            temp.append(self.x.iloc[j])
-            temp.append(self.y.iloc[j])
-            self.data.append(temp)
+    def get_log(self):
+        return self._log
 
-    def validate(self, **params):
+    def init_log(self, params):
+        for key in params:
+            self._log[key] = []
+        self._log.update({'train': [], 'val': []})
+        self.log_initialised = True
 
-        result = map(lambda data: self.build_fn(*data, **params), self.data)
+    def log(self, params, val, train):
 
-        val_result = [i[0] for i in result]
-        train_result = [i[1] for i in result]
+        if not self.log_initialised:
+            self.init_log(params)
 
-        val_avg = np.average(val_result)
-        train_avg = np.average(train_result)
+        self._log['val'].append(val)
+        self._log['train'].append(train)
 
-        return val_avg, train_avg
+        for param, value in params.iteritems():
+            self._log[param].append(value)
 
+        if val < self.best_results['val']:
+            self.best_params = params
+            self.best_results['val'] = val
+            self.best_results['train'] = train
 
-class NormalValidator:
-    def __init__(self, model_wrapper, runs, pmap=map):
-        self.pmap = pmap
-        self.runs = runs
-        self.model_wrapper = model_wrapper
+    def get_best_params(self):
+        return self.best_params
 
-    def validation_run(self, **params):
-        self.model_wrapper.set_params(**params)
-        self.model_wrapper.fit(verbose=0)
-        eval = self.model_wrapper.evaluate('val')
-        train = self.model_wrapper.evaluate('train')
-        return eval, train
-
-    def validate(self, **params):
-        self.model_wrapper.set_params(**params)
-
-        result = self.pmap(lambda param: self.validation_run(**param), [params] * self.runs)
-
-        val_result = [i[0] for i in result]
-        train_result = [i[1] for i in result]
-
-        if isinstance(val_result[0], list):
-            val_avg = np.array(val_result).mean(axis=0).tolist()
-            train_avg = np.array(train_result).mean(axis=0).tolist()
-        else:
-            val_avg = np.average(val_result)
-            train_avg = np.average(train_result)
-
-        return val_avg, train_avg
+    def get_best_results(self):
+        return self.best_results
 
 
 class Runner:
 
-    def __init__(self, model_wrapper, runs, pmap=map):
-        self.log = OrderedDict()
-        self.log_initialised = False
-        self.validator = NormalValidator(model_wrapper, runs, pmap)
-
-    def init_log(self, params):
-        for key in params:
-            self.log[key] = []
-        self.log.update({'train': [], 'val': []})
-        self.log_initialised = True
+    def __init__(self, build_fn, x, y, cv_splits, num_runs, all_params):
+        self.logger = Logger()
+        self.validator = ModelEvaluator(build_fn, x, y, cv_splits, num_runs)
+        self.param_manager = ParameterManager(all_params)
 
     def run(self, **params):
         K.clear_session()
-        if not self.log_initialised:
-            self.init_log(params)
+        params = self.param_manager.transform(params)
 
-        val, train = self.validator.validate(**params)
+        print params,
 
-        print params, val, train
+        val, train = self.validator.evaluate(**params)
 
-        self.log['val'].append(val)
-        self.log['train'].append(train)
-        for param, value in params.iteritems():
-            self.log[param].append(value)
+        print 'val', val, 'train', train
 
-        if not isinstance(val, list):
-            return val
-        else:
-            return val
+        self.logger.log(params, val, train)
+
+        return val
+
+    def get_log(self):
+        return self.logger.get_log()
 
     def get_best_params(self):
-        if isinstance(self.log['val'][0], list):
-            best_paramss = []
-            for j in range(len(self.log['val'][0])):
-                val = [i[j] for i in self.log['val']]
-                best_index = val.index(min(val))
-                best_params = OrderedDict()
-                for key, value in self.log.iteritems():
-                    if key not in ['train', 'val']:
-                        best_params[key] = value[best_index]
-                best_paramss.append(best_params)
-            return best_paramss
-        else:
-            val = self.log['val']
-            best_index = val.index(min(val))
-            best_params = OrderedDict()
-            for key, value in self.log.iteritems():
-                if key not in ['train', 'val']:
-                    best_params[key] = value[best_index]
-            return best_params
+        return self.logger.get_best_params()
+
+    def get_best_results(self):
+        return self.logger.get_best_results()
+
+    def get_params(self):
+        return self.param_manager.get_ranges()
 
 
 def get_solver(solver, solver_params, params):
@@ -163,67 +125,179 @@ def get_solver(solver, solver_params, params):
         print 'solver is {0} with num_particles={1}, num_generations={2}, max_spped={3}, phi1={4}, phi2={5}'.format(
             solver, num_particles, num_generations, max_speed, phi1, phi2)
         solver = ParticleSwarm(num_particles, num_generations, max_speed, phi1, phi2, **params)
-    # elif solver == 'nelder-mead':
-    #     ftol = params.pop('ftol', 0.0001)
-    #     max_iter = params.pop('max_iter', None)
-    #     solver = NelderMead(ftol, max_iter, **params)
-    # elif solver == 'cma-es':
-    #     num_generations = params.pop('num_generations', 100)
-    #     sigma = params.pop('sigma', 1.0)
-    #     Lambda = params.pop('lambda', None)
-    #     solver = CMA_ES(num_generations, sigma, Lambda, **params)
     else:
         raise ValueError(solver, 'is not a valid solver.')
     return solver
 
 
+def experiment_dir(data_param):
+    basedir = 'experiments'
+
+    if not os.path.exists(basedir):
+        os.mkdir(basedir)
+
+    if len(data_param['var_dict']['x']) == 1:
+        path = 'one_to_one'
+    elif len(data_param['var_dict']['x']) == 7 and len(data_param['var_dict']['y']) == 1:
+        path = 'many_to_one'
+    elif len(data_param['var_dict']['x']) == 7 and len(data_param['var_dict']['x']) == 7:
+        path = 'many_to_many'
+    else:
+        i = 0
+        path = 'experiment'
+        while os.path.exists(os.path.join(basedir, path + str(i))):
+            i += 1
+        path = os.path.join(path + str(i))
+
+    full_path = os.path.join(basedir, path)
+    if not os.path.exists(full_path):
+        os.mkdir(full_path)
+
+    return full_path
+
+
+class ParameterManager:
+    def __init__(self, param_dict):
+        self.type_dict = self._recurse_types(param_dict)
+        self.range_dict = self._recurse_ranges(param_dict)
+        self.keys = self._recurse_keys(param_dict)
+
+    def get_ranges(self):
+        return self.range_dict
+
+    def get_transforms(self):
+        return self.type_dict
+
+    def get_keys(self):
+        return self.keys
+
+    def transform(self, params):
+        transformed_params = OrderedDict()
+        for key in self.keys:
+            if key in params:
+                if key in self.type_dict and params[key] is not None:
+                    transformed_params[key] = self.type_dict[key](params[key])
+                else:
+                    transformed_params[key] = params[key]
+
+        return transformed_params
+
+    def _recurse_types(self, var):
+        if not isinstance(var, dict):
+            if isinstance(var, tuple):
+                return var[0]
+            else:
+                return None
+
+        else:
+            type_dict = OrderedDict()
+            for key, value in var.iteritems():
+                res = self._recurse_types(value)
+
+                if res is not None and not isinstance(res, dict):
+                    type_dict[key] = res
+                elif res is not None:
+                    type_dict.update(res)
+
+            return type_dict
+
+    def _recurse_ranges(self, var):
+        if not isinstance(var, dict):
+            if var is not None:
+                return var[1]
+            else:
+                return None
+
+        else:
+            range_dict = dict()
+            for key, value in var.iteritems():
+                range_dict[key] = self._recurse_ranges(value)
+
+            return range_dict
+
+    def _recurse_keys(self, var):
+        if not isinstance(var, dict):
+            return []
+
+        else:
+            keys = var.keys()
+            for key, value in var.iteritems():
+                res = self._recurse_keys(value)
+                keys.extend(res)
+            return keys
+
+
 class HyperSearch:
-    def __init__(self, solver, parallel, validation_runs, **kwargs):
-        self.solver_name = solver
-        self.solver_params = kwargs
-        self.parallel = parallel
-        self.runs = validation_runs
-
-    def hyper_data_search(self, build_fn, data_params_dict, params):
-        data_params = ParameterGrid(data_params_dict)
-        for data_param in data_params:
-            print self.hyper_search(build_fn, data_param, params)
-
-    def hyper_search(self, build_fn, data_param, params):
-        '''Perform hyperparameter search using optunity as a backend. Possible solvers include
+    def __init__(self, solver, cv_splits, validation_runs, **kwargs):
+        '''Perform hyper parameter search using optunity as backend. Possible solvers include
         grid search     args: None
         random search   args: num_evals
         particle swarm  args: num_particles, num_generations, max_speed=None, phi1=1.5, phi2=2.0
-        tpe             args: num_evals=100, seed=None
         sobol           args: num_evals, seed=None, skip=None
-        nelder-mead     args: ftol=0.0001, max_iter=None,
-
-        cma-es          args: num_generations, sigma=1.0, Lambda=None
-
         '''
+        self.solver_name = solver
+        self.solver_params = kwargs
+        self.runs = validation_runs
+        self.cv_splits = cv_splits
 
-        model_wrapper = ModelWrapper(build_fn)
-        model_wrapper.set_data_params(**data_param)
-
-        pmap = optunity.pmap if self.parallel else map
-
-        validator = Runner(model_wrapper, self.runs, pmap=pmap)
-
-        solver = get_solver(self.solver_name, self.solver_params, params)
-        res, detail = optunity.optimize(solver, validator.run, maximize=False, pmap=map)
-
-        print res
-        print detail
-        log = validator.log
-        best_params = validator.get_best_params()
-        stats = detail.stats
-
-        # Save results
-        if isinstance(best_params, list):
-            result = MultiResult(model_wrapper, log, best_params, stats)
+    def run_solver(self, f, params):
+        if self.solver_name == 'grid search':
+            solver = get_solver(self.solver_name, self.solver_params, params)
+            res, detail = optunity.optimize(solver, f, maximize=False, pmap=map)
         else:
-            result = SingleResult(model_wrapper, log, best_params, stats)
+            tree = search_spaces.SearchTree(params)
+            box = tree.to_box()
 
-        result.save('temp')
+            # we need to position the call log here
+            # because the function signature used later on is internal logic
+            f = functions.logged(f)
 
-        return result
+            # wrap the decoder and constraints for the internal search space representation
+            f = tree.wrap_decoder(f)
+            f = wrap_constraints(f, -sys.float_info.max, range_oo=box)
+
+            solver = get_solver(self.solver_name, self.solver_params, box)
+            res, detail = optunity.optimize(solver, f, maximize=False, pmap=map, max_evals=0)
+
+        return res, detail
+
+    def hyper_data_search(self, build_fn, data_params_dict, params):
+        data_params = ParameterGrid(data_params_dict)
+
+        for data_param in data_params:
+            print data_param
+            try:
+                result = self.hyper_search(build_fn, data_param, params)
+
+                exp_dir = experiment_dir(data_param)
+                result.save(exp_dir)
+
+                print result
+            except Exception as e:
+                print e
+
+    def hyper_search(self, build_fn, data_param, params):
+
+        x_train, y_train, x_test, y_test = data_utils.get_train_test_data(**data_param)
+
+        runner = Runner(build_fn, x_train, y_train, self.cv_splits, self.runs, params)
+
+        # res, detail = self.run_solver(runner.run, runner.get_params())
+        optimizer = PSOptimizer(4, 10)
+        res = optimizer.optimize(runner.run, params)
+
+        # print detail.stats
+        print runner.get_log()
+        print runner.get_best_params()
+        print runner.get_best_results()
+
+        # # Save results
+        # try:
+        #     if isinstance(best_params, list):
+        #         result = MultiResult(model_wrapper, log, best_params, stats, data_param['country'])
+        #     else:
+        #         result = SingleResult(model_wrapper, log, best_params, stats, data_param['country'])
+        # except Exception as e:
+        #     print e
+        #
+        # return result
