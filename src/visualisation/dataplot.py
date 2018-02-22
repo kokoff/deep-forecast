38e4a80642bc615726809bddcfa076833bb80b8a
skokov <sk1056@york.ac.uk>
2018-02-22 17:56:59 +0000

    Improved data plot

--- src/visualisation/dataplot.py
+++ src/visualisation/dataplot.py
@@ -1,98 +1,137 @@
 import os
 
+import matplotlib
+
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, quarter_plot
+from statsmodels.graphics.tsaplots import plot_acf as _plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf as _plot_pacf
+from statsmodels.graphics.tsaplots import quarter_plot
+from pandas.plotting import lag_plot
 from statsmodels.tsa.stattools import adfuller, kpss
+
 
 from src.utils import data_utils
 
+sns.set()
 
-class VariablePlotter:
 
-    def __init__(self, country, variable, directory='temp', output=False, show=True):
+def plot(series, label=''):
+    series.plot()
+    plt.title(label + ' Plot')
+    plt.xlabel('Time')
+    plt.ylabel(label)
+
+
+def plot_acf(series, label=''):
+    _plot_acf(series)
+    plt.title(label + ' ACF Plot')
+    plt.xlabel('Lag')
+    plt.ylabel('Autocorrelation')
+
+
+def plot_pacf(series, label=''):
+    _plot_pacf(series)
+    plt.title(label + ' PACF Plot')
+    plt.xlabel('Lag')
+    plt.ylabel('Partial Autocorrelation')
+
+
+def plot_dist(series, label=''):
+    sns.distplot(series)
+    plt.title(label + ' Distribution Plot')
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+
+
+def plot_quarter(series, label=''):
+    quarter_plot(series)
+    plt.title(label + ' Seasonal Subseries Plot')
+    plt.xlabel('Quarter')
+    plt.ylabel(label)
+
+
+def plot_lag(series, label=''):
+    lag_plot(series)
+    plt.title(label + ' Lag Plot')
+    plt.xlabel(label + ' (t)')
+    plt.ylabel(label + ' (t+1)')
+
+
+def stationary_test(series, label=''):
+    df = pd.DataFrame(index=pd.Index(('ADF', 'KPSS'), name='test'),
+                      columns=['statistic', 'p-value'])
+    df.at['ADF', :] = adfuller(series)[:2]
+    df.at['KPSS', :] = kpss(series)[:2]
+
+    return df
+
+
+class SeriesPlotter:
+
+    def __init__(self, series, label, output=None):
+        self.series = series
+        self.label = ' '.join(label.split('_'))
+        self.file_name = '_'.join(label.split(' '))
         self.output = output
-        self.show = show
-        self.dir = directory
-        self.country = country
-        if isinstance(variable, pd.Series):
-            self.data = variable
-            self.variable = variable.name
-        else:
-            self.data = data_utils.get_data_frame()[(country, variable)]
-            self.variable = variable
-        self.label = self.country + ' ' + self.variable
-
-    def path(self, name):
-        basename = '_'.join([self.country, self.variable, name])
-        return os.path.join(self.dir, basename)
+        if output is not None and not os.path.exists(output):
+            os.mkdir(output)
 
     def plot(self):
-        self.data.plot()
-        plt.title(self.label + ' Plot')
-        plt.xlabel('Time')
-        plt.ylabel(self.label)
-
-        if self.output:
-            plt.savefig(self.path('plot.pdf'))
-        plt.show()
+        plot(self.series, self.label)
+        if not self.output:
+            plt.show()
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_plot.pdf')
+            plt.savefig(plot_path)
+            plt.close()
 
     def plot_acf(self):
-        plot_acf(self.data)
-        plt.title(self.label + ' ACF Plot')
-        plt.xlabel('Lag')
-        plt.ylabel('Autocorrelation')
-
-        if self.output:
-            plt.savefig(self.path('acf.pdf'))
-        if self.show:
+        plot_acf(self.series, self.label)
+        if not self.output:
             plt.show()
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_acf.pdf')
+            plt.savefig(plot_path)
+            plt.close()
 
     def plot_pacf(self):
-        plot_pacf(self.data)
-        plt.title(self.label + ' PACF Plot')
-        plt.xlabel('Lag')
-        plt.ylabel('Partial Autocorrelation')
-
-        if self.output:
-            plt.savefig(self.path('pacf.pdf'))
-        if self.show:
+        plot_pacf(self.series, self.label)
+        if not self.output:
             plt.show()
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_pacf.pdf')
+            plt.savefig(plot_path)
+            plt.close()
 
     def plot_dist(self):
-        sns.distplot(self.data)
-        plt.title(self.label + ' Distribution Plot')
-        plt.xlabel('Value')
-        plt.ylabel('Frequency')
-
-        if self.output:
-            plt.savefig(self.path('dist.pdf'))
-        if self.show:
+        plot_dist(self.series, self.label)
+        if not self.output:
             plt.show()
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_dist.pdf')
+            plt.savefig(plot_path)
+            plt.close()
 
     def plot_quarter(self):
-        quarter_plot(self.data)
-        plt.title(self.label + ' Seasonal Subseries Plot')
-        plt.xlabel('Quarter')
-        plt.ylabel(self.label)
-
-        if self.output:
-            plt.savefig(self.path('seasonal.pdf'))
-        if self.show:
+        plot_quarter(self.series, self.label)
+        if not self.output:
             plt.show()
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_quarter.pdf')
+            plt.savefig(plot_path)
+            plt.close()
 
     def stationary_test(self):
-        df = pd.DataFrame(index=pd.Index(('ADF', 'KPSS'), name='test'),
-                          columns=['statistic', 'p-value'])
-        df.at['ADF', :] = adfuller(self.data)[:2]
-        df.at['KPSS', :] = kpss(self.data)[:2]
+        test = stationary_test(self.series, self.label)
+        if not self.output:
+            print test
+        else:
+            plot_path = os.path.join(self.output, self.file_name + '_test.csv')
+            test.to_csv(plot_path)
 
-        if self.output:
-            df.to_csv(self.path('tests.csv'))
-        if self.show:
-            print df
-        return df
+        return
 
     def plot_all(self):
         self.plot()
@@ -105,90 +144,87 @@ class VariablePlotter:
 
 class DataPlotter:
 
-    def __init__(self, directory='temp', output=False, show=True):
+    def __init__(self, data_frame, country, output=None):
         self.output = output
-        self.show = show
-        self.directory = directory
-        self.data = data_utils.get_data_frame()
+        if output is not None and not os.path.exists(output):
+            os.mkdir(output)
+
+        self.data = data_frame
+        self.country = country
+        self.columns = data_frame.columns.tolist()
+        self.labels = [' '.join([country, col]) for col in self.columns]
         self.plotters = {}
-        for tup in self.data.columns.tolist():
-            self.plotters[tup] = VariablePlotter(tup[0], tup[1], directory=directory, output=output, show=show)
 
-    def plot_matrix(self):
-        data = data_utils.get_flat_data_frame()
-        x_vars = data.columns[len(data.columns) / 2:]
-        y_vars = data.columns[:len(data.columns) / 2]
-        sns.pairplot(data, x_vars=x_vars, y_vars=y_vars)
-        if self.output:
-            plt.savefig(os.path.join(self.directory, 'matrix.pdf'))
-        if self.show:
+        for column, label in zip(self.columns, self.labels):
+            self.plotters[column] = SeriesPlotter(self.data[column], label, output=output)
+
+    def plot_matrix(self, labels=None):
+        sns.pairplot(self.data)
+        if not self.output:
             plt.show()
+        else:
+            plot_file = os.path.join(self.output, self.country + '_pairplot.pdf')
+            plt.savefig(plot_file)
+            plt.close()
 
-        for country in self.data.columns.levels[0]:
-            sns.pairplot(self.data[country])
-            if self.output:
-                plt.savefig(os.path.join(self.directory, country + '_matrix.pdf'))
-            if self.show:
-                plt.show()
+    def plot(self, labels=None):
+        if labels is None:
+            labels = self.columns
 
-    def _get_params(self, countries=None, variables=None):
-        if countries is None:
-            countries = list(self.data.columns.levels[0])
-        elif not isinstance(countries, list):
-            countries = [countries]
+        for label in labels:
+            self.plotters[label].plot()
 
-        if variables is None:
-            variables = list(self.data.columns.levels[1])
-        elif not isinstance(variables, list):
-            variables = [variables]
+    def plot_acf(self, labels):
+        labels = self.columns if labels is None else labels
 
-        params = []
-        for i in countries:
-            for j in variables:
-                params.append((i, j))
+        for label in labels:
+            self.plotters[label].plot_acf()
 
-        return params
+    def plot_pacf(self, labels=None):
+        if labels is None:
+            labels = self.columns
 
-    def plot(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot()
+        for label in labels:
+            self.plotters[label].plot_pacf()
 
-    def plot_acf(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot_acf()
+    def plot_dist(self, labels=None):
+        if labels is None:
+            labels = self.columns
 
-    def plot_pacf(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot_pacf()
+        for label in labels:
+            self.plotters[label].plot_dist()
 
-    def plot_dist(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot_dist()
+    def plot_quarter(self, labels=None):
+        if labels is None:
+            labels = self.columns
 
-    def plot_quarter(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot_quarter()
+        for label in labels:
+            self.plotters[label].plot_quarter()
 
-    def stationary_test(self, countries=None, variables=None):
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].stationary_test()
+    def stationary_test(self, labels=None):
+        if labels is None:
+            labels = self.columns
 
-    def plot_all(self, countries=None, variables=None):
+        for label in labels:
+            self.plotters[label].stationary_test()
+
+    def plot_all(self, labels=None):
+        if labels is None:
+            labels = self.columns
+
+        for label in labels:
+            self.plotters[label].plot_all()
+
         self.plot_matrix()
-
-        params = self._get_params(countries, variables)
-        for param in params:
-            self.plotters[param].plot_all()
 
 
 # TODO Implement cross correlation matrix for each variable with different lags of other variables
 
 if __name__ == '__main__':
-    plotter = DataPlotter(output=True)
+    series = data_utils.get_ea_data()
+    # plotter = SeriesPlotter(series, label='EA CPI')
+    # plotter.plot_all()
+    # plot_lag(series, 'EA CPI')
+    # plt.show()
+    plotter = DataPlotter(series, 'EA')
     plotter.plot_all()
