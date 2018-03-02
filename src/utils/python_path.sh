af30f1535b113a5000ea92e9e943522522b30740
skokov <sk1056@york.ac.uk>
2018-03-02 21:19:28 +0000

    Fix path

--- src/utils/python_path.sh
+++ src/utils/python_path.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PYTHONPATH=${PYTHONPATH}: $(realpath$DIR/../../)
+PYTHONPATH=${PYTHONPATH}:$DIR/../../
 echo $PYTHONPATH
