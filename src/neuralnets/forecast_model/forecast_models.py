c55bc3080e3536932af50bf2771282a4b42c909c
skokov <sk1056@york.ac.uk>
2018-03-15 13:32:03 +0000

    Changed input output shapes of forecast model

--- src/neuralnets/forecast_model/forecast_models.py
+++ src/neuralnets/forecast_model/forecast_models.py
@@ -33,27 +33,45 @@ def create_output_layers(num_outputs, output_size, prev_layers):
         return outputs
 
 
-class ForecastModel(Model):
+def split_data(data, splits):
+    if isinstance(data, pd.DataFrame):
+        data = data.values
 
-    def _split_data(self, x, y=None):
+    if splits > 1:
+        return np.split(data, splits, axis=1)
+    else:
+        return data
+
+
+class ForecastModel(Model):
+    def __init__(self, inputs, outputs):
+        super(ForecastModel, self).__init__(inputs, outputs)
+
         if isinstance(self.input, list):
-            x = np.split(x, len(self.input), axis=1)
-        if y is not None and isinstance(self.output, list):
-            y = np.split(y, len(self.output), axis=1)
-        if y is not None:
-            return x, y
+            self.x_lags = int(self.input[0].shape[1])
+            self.x_vars = len(self.input)
         else:
-            return x
+            self.x_lags = int(self.input.shape[1])
+            self.x_vars = 1
+
+        if isinstance(self.output, list):
+            self.y_lags = int(self.output[0].shape[1])
+            self.y_vars = len(self.output)
+        else:
+            self.y_lags = int(self.output.shape[1])
+            self.y_vars = 1
 
     def fit(self, x=None, y=None, batch_size=None, epochs=1, verbose=0, callbacks=None, validation_split=0.,
             validation_data=None, shuffle=False, class_weight=None, sample_weight=None, initial_epoch=0,
             steps_per_epoch=None, validation_steps=None, **kwargs):
 
         # Split data if multi input/output model
-        x, y = self._split_data(x, y)
+        x = split_data(x, self.x_vars)
+        y = split_data(y, self.y_vars)
 
         if validation_data:
-            x_val, y_val = self._split_data(validation_data[0], validation_data[1])
+            x_val = split_data(validation_data[0], self.x_vars)
+            y_val = split_data(validation_data[1], self.y_vars)
             validation_data = [x_val, y_val]
 
         return super(ForecastModel, self).fit(x, y, batch_size, epochs, verbose, callbacks, validation_split,
@@ -63,63 +81,50 @@ class ForecastModel(Model):
 
     def predict(self, x, batch_size=None, verbose=0, steps=None):
         # Split data if multi input/output model
-        x = self._split_data(x)
+        x = split_data(x, self.x_vars)
 
-        return super(ForecastModel, self).predict(x, batch_size, verbose, steps)
+        y = super(ForecastModel, self).predict(x, batch_size, verbose, steps)
+
+        y = np.reshape(y, (self.y_vars, -1, self.y_lags))
+        return np.array(y)
 
     def evaluate(self, x=None, y=None, batch_size=None, verbose=0, sample_weight=None, steps=None):
-        x, y = self._split_data(x, y)
+        x = split_data(x, self.x_vars)
+        y = split_data(y, self.y_vars)
         return super(ForecastModel, self).evaluate(x, y, batch_size, verbose, sample_weight, steps)
 
     def forecast(self, x, y, batch_size=None, verbose=0, steps=None):
         x_labels = x.columns.levels[0].tolist()
         y_labels = y.columns.levels[0].tolist()
 
-        x, y = self._split_data(x, y)
+        x = split_data(x, self.x_vars)
+        y = split_data(y, self.y_vars)
 
-        if isinstance(self.input, list):
-            x_lags = int(self.input[0].shape[1])
-            x_vars = len(self.input)
-            x_matrix = np.array([i.as_matrix() for i in x])
-        else:
-            x_lags = int(self.input.shape[1])
-            x_vars = 1
-            x_matrix = np.reshape(x.as_matrix(), (x_vars, len(x), x_lags))
-
-        if isinstance(self.output, list):
-            y_lags = int(self.output[0].shape[1])
-            y_vars = len(self.output)
-            y_true_matrix = np.array([i.as_matrix() for i in y])
-        else:
-            y_lags = int(self.output.shape[1])
-            y_vars = 1
-            y_true_matrix = np.reshape(y.as_matrix(), (y_vars, len(y), y_lags))
+        x_matrix = np.reshape(x, (self.x_vars, -1, self.x_lags))
+        y_true_matrix = np.reshape(y, (self.y_vars, -1, self.y_lags))
 
         data_len = x_matrix.shape[1]
-        y_matrix = np.zeros((y_vars, data_len, y_lags))
+        y_matrix = np.zeros((self.y_vars, data_len, self.y_lags))
 
         input = x_matrix[:, 0:1]
         var_indexes = [x_labels.index(i) for i in y_labels if i in x_labels]
 
         for i in range(data_len):
             if i > 0:
-                new_input = x_matrix[0:x_vars, i:i + 1].copy()
-                old = input[var_indexes, :, :-y_lags]
+                new_input = x_matrix[0:self.x_vars, i:i + 1].copy()
+                old = input[var_indexes, :, :-self.y_lags]
                 new_input[var_indexes] = np.concatenate([output, old], axis=-1)
                 input = new_input
 
             output = super(ForecastModel, self).predict(list(input), batch_size, verbose, steps)
-            output = np.reshape(output, (y_vars, 1, y_lags))
+            output = np.reshape(output, (self.y_vars, 1, self.y_lags))
 
-            output = y_true_matrix[:, i:i + 1]
+            # output = y_true_matrix[:, i:i + 1]
             y_matrix[:, i:i + 1] = output
 
             # print list(input)
             # print list(x_matrix[:, i:i + 1])
 
-        y_matrix = np.squeeze(y_matrix)
-        if y_vars == 1:
-            y_matrix = np.expand_dims(y_matrix, -1)
         return y_matrix
 
     def evaluate_forecast(self, x, y, batch_size=None, verbose=0, steps=None):
@@ -209,17 +214,20 @@ def one_one():
     from src.utils import data_utils
     from keras import layers
 
-    x, y = data_utils.get_data_in_shape('EA', (['CPI'], ['CPI']), 1)
+    x, y = data_utils.get_data_in_shape('EA', (['CPI'], ['CPI']), 2, 2)
     x_train, x_val, x_test, = data_utils.train_val_test_split(x, 12, 12)
     y_train, y_val, y_test = data_utils.train_val_test_split(y, 12, 12)
 
-    input = Input(shape=(1,))
-    layer = Dense(1)(input)
+    input = Input(shape=(2,))
+    layer = Dense(2)(input)
 
     model = ForecastModel(inputs=input, outputs=layer)
     model.compile(optimizer='adam', loss='mse')
 
-    print model.forecast(x_val, y_val)
+    print 'one one'
+    model.fit(x_train, y_train)
+    print model.predict(x_val).shape
+    print model.forecast(x_val, y_val).shape
 
 
 def m_m_m():
@@ -250,7 +258,7 @@ def many_one():
     from src.utils import data_utils
     from keras import layers
 
-    x, y = data_utils.get_data_in_shape('EA', (['CPI', 'GDP'], ['CPI']), 2)
+    x, y = data_utils.get_data_in_shape('EA', (['CPI', 'GDP'], ['CPI']), 2, 2)
     x_train, x_val, x_test, = data_utils.train_val_test_split(x, 12, 12)
     y_train, y_val, y_test = data_utils.train_val_test_split(y, 12, 12)
 
@@ -258,33 +266,39 @@ def many_one():
     input2 = Input(shape=(2,))
 
     layer = layers.concatenate([input1, input2])
-    layer = Dense(1)(layer)
+    layer = Dense(2)(layer)
 
     model = ForecastModel(inputs=[input1, input2], outputs=layer)
     model.compile(optimizer='adam', loss='mse')
 
-    print model.forecast(x_val, y_val)
+    print 'many one'
+    model.fit(x_train, y_train)
+    print model.predict(x_val).shape
+    print model.forecast(x_val, y_val).shape
 
 
 def many_many():
     from src.utils import data_utils
     from keras import layers
 
-    x, y = data_utils.get_data_in_shape('EA', (['CPI', 'GDP'], ['CPI', 'GDP']), 6)
+    x, y = data_utils.get_data_in_shape('EA', (['CPI', 'GDP'], ['CPI', 'GDP']), 2, 2)
     x_train, x_val, x_test, = data_utils.train_val_test_split(x, 12, 12)
     y_train, y_val, y_test = data_utils.train_val_test_split(y, 12, 12)
 
-    input1 = Input(shape=(6,))
-    input2 = Input(shape=(6,))
+    input1 = Input(shape=(2,))
+    input2 = Input(shape=(2,))
 
     layer = layers.concatenate([input1, input2])
-    layer1 = Dense(1)(layer)
-    layer2 = Dense(1)(layer)
+    layer1 = Dense(2)(layer)
+    layer2 = Dense(2)(layer)
 
     model = ForecastModel(inputs=[input1, input2], outputs=[layer1, layer2])
     model.compile(optimizer='adam', loss='mse')
 
-    print model.forecast(x_val, y_val)
+    print 'many many'
+    model.fit(x_train, y_train)
+    print model.predict(x_val).shape
+    print model.forecast(x_val, y_val).shape
 
 
 def main1():
@@ -298,8 +312,8 @@ def main1():
 
 
 if __name__ == '__main__':
-    # one_one()
-    # many_one()
-    # many_many()
+    one_one()
+    many_one()
+    many_many()
     # main()
-    m_m_m()
+    # m_m_m()
